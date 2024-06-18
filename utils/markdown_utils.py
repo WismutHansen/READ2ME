a36923a3be8a49a945663f0a9cd319c01ee094e1
Tommy Falkowski <wismuthansen@gmail.com>
2024-06-18 21:36:38 +0200

    added endpoint for text

--- utils/markdown_utils.py
+++ utils/markdown_utils.py
@@ -1,7 +1,8 @@
 import os
 
 
-def write_markdown_file(md_file_path, text, url):
+def write_markdown_file(md_file_path, text, url=None):
     with open(md_file_path, "w") as md_file_handle:
         md_file_handle.write(text)
-        md_file_handle.write(f"\n\nSource: {url}")
+        if url:
+            md_file_handle.write(f"\n\nSource: {url}")
