950cf6954da73cf9210227de152a79d762e7b5f2
Tommy Falkowski <wismuthansen@gmail.com>
2024-06-21 18:27:24 +0200

    improved async function handling2

--- utils/env.py
+++ utils/env.py
@@ -1,27 +1,46 @@
 import os
 from dotenv import load_dotenv
 
-
 def setup_env():
     def check_env_file_exists(directory="."):
         env_file_path = os.path.join(directory, ".env")
         return os.path.isfile(env_file_path)
 
+    def check_if_files_exist(directory="."):
+        sources_file_path = os.path.join(directory, "sources.txt")
+        keywords_file_path = os.path.join(directory, "keywords.txt")
+        return os.path.isfile(sources_file_path) and os.path.isfile(keywords_file_path)
+
     if check_env_file_exists():
         load_dotenv()
-        output_dir = os.getenv("OUTPUT_DIR")
-        task_file = os.getenv("TASK_FILE")
-        img_pth = os.getenv("IMG_PATH")
+        output_dir = os.getenv("OUTPUT_DIR", "Output")
+        task_file = os.getenv("TASK_FILE", "tasks.txt")
+        img_pth = os.getenv("IMG_PATH", "front.jpg")
+        sources_file_path = os.getenv("SOURCES_FILE", "sources.txt")
+        keywords_file_path = os.getenv("KEYWORDS_FILE", "keywords.txt")
     else:
         output_dir = "Output"
         task_file = "tasks.txt"
         img_pth = "front.jpg"
+        sources_file_path = "sources.txt"
+        keywords_file_path = "keywords.txt"
 
-    return output_dir, task_file, img_pth
+    if not os.path.isfile(sources_file_path):
+        print("Creating sources.txt")
+        with open(sources_file_path, 'w') as f:
+            pass  # This creates an empty sources.txt file
 
+    if not os.path.isfile(keywords_file_path):
+        print("Creating keywords.txt")
+        with open(keywords_file_path, 'w') as f:
+            pass  # This creates an empty keywords.txt file
+
+    return output_dir, task_file, img_pth, sources_file_path, keywords_file_path
 
 if __name__ == "__main__":
-    output_dir, task_file, img_pth = setup_env()
+    output_dir, task_file, img_pth, sources_file_path, keywords_file_path = setup_env()
     print(output_dir)
     print(task_file)
     print(img_pth)
+    print(sources_file_path)
+    print(keywords_file_path)
