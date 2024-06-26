d83ba8e570137b5dd46fe76b2f4cb2ac22c42654
Wismut Hansen <wismuthansen@gmail.com>
2024-06-26 07:15:35 +0200

    fixed error in main with datetime that caused text extraction to fail

--- utils/logging_utils.py
+++ utils/logging_utils.py
@@ -1,9 +1,17 @@
 import logging
+import sys
 
+class ForcedFlushHandler(logging.FileHandler):
+    def emit(self, record):
+        super().emit(record)
+        self.flush()
 
 def setup_logging(log_file_path):
     logging.basicConfig(
         level=logging.INFO,
         format="%(asctime)s - %(levelname)s - %(message)s",
-        handlers=[logging.FileHandler(log_file_path), logging.StreamHandler()],
+        handlers=[
+            ForcedFlushHandler(log_file_path),
+            logging.StreamHandler(sys.stdout)
+        ],
     )
