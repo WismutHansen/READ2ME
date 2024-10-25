2e37632b7f81494b3bd6a5b02f7198c85d9a0526
Tommy Falkowski <wismuthansen@gmail.com>
2024-10-25 15:18:28 +0200

    major refactor in progress

--- utils/F5_TTS_bak/model/__init__.py
+++
@@ -1,5 +0,0 @@
-from utils.F5_TTS.model.cfm import CFM
-from utils.F5_TTS.model.backbones.unett import UNetT
-from utils.F5_TTS.model.backbones.dit import DiT
-from utils.F5_TTS.model.backbones.mmdit import MMDiT
-from utils.F5_TTS.model.trainer import Trainer
