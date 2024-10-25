991eba7c66e4e13ac8a03253afa99b928edaca8d
Tommy Falkowski <wismuthansen@gmail.com>
2024-10-25 15:18:54 +0200

    major refactor in progress

---
+++ utils/F5_TTS/model/__init__.py
@@ -0,0 +1,10 @@
+from utils.F5_TTS.model.cfm import CFM
+
+from utils.F5_TTS.model.backbones.unett import UNetT
+from utils.F5_TTS.model.backbones.dit import DiT
+from utils.F5_TTS.model.backbones.mmdit import MMDiT
+
+from utils.F5_TTS.model.trainer import Trainer
+
+
+__all__ = ["CFM", "UNetT", "DiT", "MMDiT", "Trainer"]
