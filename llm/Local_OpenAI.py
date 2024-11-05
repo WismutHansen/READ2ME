ff7833580d4ac3413e7913dfa6d02b8ea7421f7c
kuchikihater <sandrodavid50@gmail.com>
2024-11-05 11:22:20 +0100

    llm_openai

--- llm/Local_OpenAI.py
+++ llm/Local_OpenAI.py
@@ -1,28 +1,31 @@
-from openai import OpenAI
+from langchain_openai import ChatOpenAI
 from dotenv import load_dotenv
 import os
-
 
 load_dotenv()
 openai_base_url = os.getenv("OPENAI_BASE_URL", "http://10.161.141.2:1234/v1")
 openai_api_key = os.getenv("OPENAI_API_KEY")
 model_name = os.getenv("MODEL_NAME")
 
-
-client = OpenAI(base_url=openai_base_url, api_key=openai_api_key)
+client = ChatOpenAI(
+    base_url=openai_base_url,
+    api_key=openai_api_key,
+    model=model_name,
+    streaming=True
+)
 
 
 def ask_LLM(user_message, system_message="You are a helpful assistant"):
-    stream = client.chat.completions.create(
-        model=model_name,
-        messages=[{"role": "user", "content": user_message}],
-        stream=True,
-    )
+    messages = [
+        {"role": "system", "content": system_message},
+        {"role": "user", "content": user_message}
+    ]
 
     assistant_message = ""
-    for chunk in stream:
-        if chunk.choices[0].delta.content is not None:
-            assistant_message += chunk.choices[0].delta.content
+    for chunk in client.stream(input=messages, stream=True):
+        if hasattr(chunk, "content"):
+            assistant_message += chunk.content
+
     return assistant_message
 
 
@@ -39,4 +42,3 @@ if __name__ == "__main__":
             print("\nAssistant\n--------\n", end="")
             print(answer)
         history += f"User: {question}\nAssistant: {answer}\n"
-
