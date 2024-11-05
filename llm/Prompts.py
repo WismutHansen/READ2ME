54996db20fecbd01a04760f17afb39c857f3695d
kuchikihater <sandrodavid50@gmail.com>
2024-11-05 11:18:02 +0100

    tech_task

--- llm/Prompts.py
+++ llm/Prompts.py
@@ -1,15 +1,55 @@
-pod = """\n--------\n
-Using the above text, please generate a detailed and intriguing podcast transcription between two speakers,
-a man and a woman. The conversation should flow naturally and explore the content of the seed text in an engaging way. 
-Include filler words like "um," "you know," and "like" to make it sound as human as possible.
-Speakers should be identifiable by their names at the beginning of their lines (e.g., "speaker1:" and "speaker2:").
-Do not include any additional annotations such as stage directions or sound effects e.g. (laughing).
-The transcription should read like a real conversation between two people discussing the topic.
-No names are used in the dialogue.
-"""
-title_prompt = """\n--------\n
-Create a 3-word title for the above text. Output only the 3-word title and nothing else!
-"""
-story_mode = """\n--------\n
-turn the above text into an exciting story for children!
-"""
+from langchain_core.prompts import PromptTemplate
+
+pod = PromptTemplate.from_template(
+    """
+    Using the text highlighted between ###, please generate a detailed and intriguing podcast transcription between two speakers,
+    a man and a woman. The conversation should flow naturally and explore the content of the seed text in an engaging way. 
+    Include filler words like "um," "you know," and "like" to make it sound as human as possible.
+    Speakers should be identifiable by their names at the beginning of their lines (e.g., "speaker1:" and "speaker2:").
+    Do not include any additional annotations such as stage directions or sound effects e.g. (laughing).
+    The transcription should read like a real conversation between two people discussing the topic.
+    No names are used in the dialogue.
+    ###
+    {text}
+    ###
+    Provide your response in the following JSON format:
+    {format_instructions}
+    """
+)
+
+title_prompt = PromptTemplate.from_template(
+    """
+    Create a 3-word title for the text highlighted between ###. Output only the 3-word title and nothing else!
+    ###
+    {text}
+    ###
+    """
+)
+
+story_mode = PromptTemplate.from_template(
+    """
+    turn the text highlighted between ### into an exciting story for children!
+    ###
+    {text}
+    ###
+    """
+)
+
+story_mode_with_language = PromptTemplate.from_template(
+    """
+    turn the text highlighted between ### into an exciting story for children in {language}!
+    ###
+    {text}
+    ###
+    """
+)
+
+summ_prompt = PromptTemplate.from_template(
+    """
+    Return a concise summary for the text highlighted between ###, without referencing the text 
+    or mentioning 'in the text' or similar phrases. Keep the tone and perspective of the original text.
+    ###
+    {text}
+    ###
+    """
+)
